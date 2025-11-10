c2f4469aa0894edf938285a58fc8b1ed01813896
rifkiaf <ahmadfahreizirifki@gmail.com>
2025-11-11 03:57:50 +0700

    Fix index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD World 🚀" //);
+  res.send("Hello CI/CD World 🚀");
 });
 
 app.listen(PORT, () => {
